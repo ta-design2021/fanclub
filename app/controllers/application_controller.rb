e18ed37fb0d8eee0df7f954b6f5daf7fec9a70c2
ta-design2021 <takenori.aino@gmail.com>
2023-10-13 16:00:24 +0900

    ストロングパラメーターの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
+  end
+
+  # フォームから送信されるパラメーターを受け取るために、ストロングパラメーターを設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :gender_id, :birth_date, :phone_number, :postal_code, :prefecture_id, :city, :street_address, :membership_plan_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'top/index'
+  # トップページにアクセスしたときのルート
+  root "top#index"
+  # マイページに遷移するルート
 end
